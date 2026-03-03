164c7ce771a0808e4ef8e0a605775cc2c5a279e9
Arpit <123756415+ar-py-grim@users.noreply.github.com>
2026-03-02 19:27:18 -0500

    Add files via upload

--- src/py_moveit2/pymoveit2/robots/bot.py
+++ src/py_moveit2/pymoveit2/robots/bot.py
@@ -2,22 +2,20 @@ from typing import List
 
 MOVE_GROUP_ARM: str = "arm"
 MOVE_GROUP_GRIPPER: str = "gripper"
-                                            # yaw angles 
-                                        # in order as in gripper_joint_names()
-OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [-1.57, 1.57]
-CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [0.0, 0.0]
+                                        
+# yaw angles in order as in gripper_joint_names()
+OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [-1.57]
+CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [0.0]
+OPEN_GRIPPER_PICK_JOINT_POSITIONS: List[float] = [-0.5]
+CLOSED_GRIPPER_PICK_JOINT_POSITIONS: List[float] = [-0.1]
 
-def joint_names() -> List[str]:
+def arm_joint_names() -> List[str]:
 
 # order of joints found using ros2 topic echo /joint_states
-
     return["joint_1",
         "joint_2",
-        "joint_4",
         "joint_3",
-        "joint_5_mimic"]
-        # "joint_5"]
-
+        ]
 
 def base_link_name() -> str:
     return "base_link"
@@ -26,6 +24,8 @@ def end_effector_name() -> str:
     return "gripper_right"
 
 def gripper_joint_names() -> List[str]:
-    return ["joint_4", 
-            "joint_5_mimic"]
-            # "joint_5"]
+    return ["joint_4"]
+
+def joint_names() -> List[str]:
+    """All robot joints"""
+    return arm_joint_names() + gripper_joint_names()
